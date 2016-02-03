cb9ed42b55e219d28c7b1c043b15a26cbee94d27
Tiago Heineck <Tiago Heineck>
2016-02-03 20:25:30 -0200

    removi index

---
+++ database/migrations/2016_02_03_222201_remove.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Remove extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('users_facebook_id_unique');
+        });
+    }
+}
